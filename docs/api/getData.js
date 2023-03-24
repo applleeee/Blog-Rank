77a119554d02ca7a705b2dd58f4faebed8b86dc4
applleeee <warm9806@gmail.com>
2023-03-24 18:23:37 +0900

    test

--- docs/api/getData.js
+++ docs/api/getData.js
@@ -6,6 +6,7 @@ export default async function getData(request, response) {
       const res = await axios.get(request.body.url);
       response.send(res.data).status(200);
    } catch (error) {
+      console.log("erororororor");
       console.error(error);
       response.status(error.response?.status || 500).end(error.message);
    }
