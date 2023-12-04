3fa4230f90c9ae33012dec6ad5c40dc7427c02bc
applleeee <warm9806@gmail.com>
2023-12-04 18:42:46 +0900

    user-agent 설정

--- api/getData.js
+++ api/getData.js
@@ -1,10 +1,15 @@
 export default async function getData(request, response) {
-   try {
-      const res = await fetch(request.body.url);
-      const data = await res.text();
-      response.send(data).status(200);
-   } catch (error) {
-      console.error(error);
-      response.status(error.response?.status || 500).end(error.message);
-   }
+  try {
+    const res = await fetch(request.body.url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
+      },
+    });
+    const data = await res.text();
+    response.send(data).status(200);
+  } catch (error) {
+    console.error(error);
+    response.status(error.response?.status || 500).end(error.message);
+  }
 }
